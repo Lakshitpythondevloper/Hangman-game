16cb80631d530c8a80942768eee0c080e70e2817
Lakshit Sharma <pythondevloper78@outlook.com>
2025-06-24 09:47:15 -0700

    Add files via upload

---
+++ Hangman_words.py
@@ -0,0 +1,8 @@
+word_list = ["Mouse","Cat","Hello","Python","Programming","Code","Computer","Keyboard","Screen"
+             ,"apple", "banana", "grape", "orange", "pear", "peach", "plum", "kiwi", "melon", "berry",
+             "cherry", "mango", "papaya", "pineapple", "coconut", "watermelon", "apricot", "fig",
+             "hey", "hi", "bye", "goodbye", "welcome", "thanks", "please", "sorry", "yes", "no",
+              "maybe", "okay", "sure", "great", "cool", "awesome", "fantastic", "amazing",
+             "wonderful", "excellent", "superb", "brilliant", "outstanding", "remarkable", "impressive",
+             "extraordinary", "exceptional", "fabulous", "magnificent", "spectacular", "stunning",
+             "breathtaking", "jaw-dropping", "mind-blowing", "phenomenal", "incredible", "unbelievable",]
